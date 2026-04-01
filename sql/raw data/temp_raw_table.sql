49f8a43e531da56ffaf7d18c7845b6977ca5354e
Prafull Wahatule <prafullwahatule@gmail.com>
2026-04-01 09:01:19 +0530

    Work

--- sql/raw data/temp_raw_table.sql
+++ sql/raw data/temp_raw_table.sql
@@ -1,18 +1,11 @@
 USE bookstore_db;
 
-CREATE TABLE raw_sales (
-    sale_id INT AUTO_INCREMENT PRIMARY KEY,
-    sale_date DATE,
-    store_id VARCHAR(10),
-    channel VARCHAR(20),
-    transactions INT,
-    revenue DECIMAL(10,2),
-    gst_collected DECIMAL(10,2),
-    net_revenue DECIMAL(10,2)
-);
-
-
-CREATE TABLE raw_inventory (
-	inventory_id INT AUTO_INCREMENT PRIMARY KEY,
-    
+CREATE TABLE checking_balanced(
+	date DATE,
+	description VARCHAR(50),
+	deposit INT,
+	withdrawal INT,
+	account_id INT,
+	net_change INT,
+	rolling_balance INT
 );
